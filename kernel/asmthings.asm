a75eafcaf906671224b708e5804dbf6cabad0679
esdraslov <esdraslov@gmail.com>
2026-06-06 11:51:49 -0300

    ausdfuh

--- kernel/asmthings.asm
+++ kernel/asmthings.asm
@@ -5,7 +5,7 @@ gdt_flush:
     mov eax, [esp + 4]  ; Get the pointer to gdt_ptr from the C argument
     lgdt [eax]          ; Load the GDT
 
-    jmp 0x08:.flush     ; 0x08 is the offset to our code segment. Far jump!
+    jmp far 0x08:.flush     ; 0x08 is the offset to our code segment. Far jump!
 .flush:
     mov ax, 0x10        ; 0x10 is the offset to our data segment
     mov ds, ax          ; Load all data segment registers
